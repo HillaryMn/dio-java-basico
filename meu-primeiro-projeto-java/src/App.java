b879d09c569c3de6a6fde68062c09ec34b0d3a90
Kevyn Santos <educationalK@gmail.com>
2025-02-26 19:46:22 -0300

    correçao

--- meu-primeiro-projeto-java/src/App.java
+++ meu-primeiro-projeto-java/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Estou feliz de participar do decolar tech 2025");
+        System.out.println("Estou feliz de participar do decola tech 2025");
     }
 }
